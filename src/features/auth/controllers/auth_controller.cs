aeb09bd3521e84b5b23a4be73c7e7554299d55ad
cobyzero <sebastianabalsalazar@gmail.com>
2025-02-08 12:40:43 -0500

    feat: add try in methods

--- src/features/auth/controllers/auth_controller.cs
+++ src/features/auth/controllers/auth_controller.cs
@@ -17,29 +17,40 @@ public class AuthController : ControllerBase
     [HttpPost("login")]
     public IActionResult Login([FromBody] LoginRequestModel model)
     {
+        try
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Email == model.Email && u.Password == model.Password);
+            if (user == null)
+                return Ok(ResponseUtil.Error(null, "Invalid email or password"));
 
-        var user = _context.Users.FirstOrDefault(u => u.Email == model.Email && u.Password == model.Password);
-        if (user == null)
-            return Ok(ResponseUtil.Error(null, "Invalid email or password"));
 
-
-        var jwtUtil = new JwtUtil(_configuration);
-        return Ok(ResponseUtil.Success(jwtUtil.GenerateToken(user.Email), "Login successful"));
+            var jwtUtil = new JwtUtil(_configuration);
+            return Ok(ResponseUtil.Success(jwtUtil.GenerateToken(user.Email), "Login successful"));
+        }
+        catch (System.Exception)
+        {
+            return Ok(ResponseUtil.Error(null, "Something went wrong"));
+        }
     }
 
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegisterRequestModel model)
     {
-        var user = new User
+        try
         {
-            Email = model.Email,
-            Password = model.Password,
-            Name = model.Name
-        };
-        await _context.Users.AddAsync(user);
-        await _context.SaveChangesAsync();
-        return Ok(ResponseUtil.Success(user, "Register successful"));
+            var user = new User
+            {
+                Email = model.Email,
+                Password = model.Password,
+                Name = model.Name
+            };
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok(ResponseUtil.Success(user, "Register successful"));
+        }
+        catch (System.Exception)
+        {
+            return Ok(ResponseUtil.Error(null, "Something went wrong"));
+        }
     }
-
-
 }
